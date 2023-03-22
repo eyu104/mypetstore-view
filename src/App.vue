12e540de0b006e8303dbd464a0fd84b04ceb7842
YiXiao <imgynks@outlook.com>
2023-03-22 08:27:30 +0800

    注册窗口大致页面，功能未写

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import  Header  from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import LoginForm from "./components/LoginForm.vue";
 
 </script>
 
@@ -9,17 +10,17 @@ import Footer from './components/Footer.vue'
  
     <div class="common-layout">
       <el-container>
-          <el-header><Header /></el-header>
-          <el-main>
-            <div class="main">
-                <RouterView />
-            </div>
-          </el-main>
-          <el-footer>
-            <div class="footer">
-              <Footer />
-            </div>
-          </el-footer>
+        <el-header><Header /></el-header>
+        <el-main>
+          <div class="main">
+              <RouterView />
+          </div>
+        </el-main>
+        <el-footer>
+          <div class="footer">
+            <Footer />
+          </div>
+        </el-footer>
       </el-container>
     </div>
 
@@ -30,7 +31,10 @@ import Footer from './components/Footer.vue'
 <style scoped>
 
 /* 测试 */
-
+.main {
+  height: calc(100vh - 168px);
+  
+}
 
 
 
