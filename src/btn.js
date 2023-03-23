606f96bfa3d76e90ffb77c64244035b67ec4b552
GaryFan <2985708292@qq.com>
2023-03-23 18:17:23 +0800

    轮播以及首页

---
+++ src/btn.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector('.btn');
+
+btn.addEventListener('mouseover',(event)=>{
+    const x = (event.pageX - btn.offsetLeft)
+    const y = (event.pageY - btn.offsetTop)
+
+    btn.style.setProperty("--xPos",x + "px")
+    btn.style.setProperty("--yPos",y + "px")
+})
