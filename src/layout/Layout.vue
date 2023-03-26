8c52e558adabb058ebc7e51315172c087dfc7f7c
GaryFan <2985708292@qq.com>
2023-03-26 12:34:19 +0800

    fix

---
+++ src/layout/Layout.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-container>
+
+    <el-header>
+      <Header />
+    </el-header>
+
+    <el-main>
+      <div class="main">
+        <RouterView />
+      </div>
+    </el-main>
+
+    <el-footer>
+      <div class="footer">
+        <Footer />
+      </div>
+    </el-footer>
+
+  </el-container>
+
+
+</template>
+
+<script>
+
+import  Header  from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: "Layout",
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
